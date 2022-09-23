320091d54d5e2c2b806660b48f1b67707463b441
Mokoth <onyach.okoth1@outlook.com>
2022-09-23 12:30:22 +0300

    Few ads to filw

--- Head-First-C/func.c
+++ Head-First-C/func.c
@@ -4,6 +4,34 @@ void greet(int num){
     printf("Hello, Mom! %d\n", num);
 }
 
-int main(){
-    greet(123);
+// function for a square
+void square(int num){
+    int squared = num * num;
+    printf("The square of %d is %d \n" , num, squared);
+
 }
+
+
+// function to print string of characters
+void fortune_cookie(char msg[]){
+    printf("Message reads: %s \n", msg);
+    printf("Message occupies %i bytes \n", sizeof(msg));
+}
+
+int main(){
+
+    greet(123);
+
+    square(122);
+
+    char message[] = "Cookies makes you fat!";
+    fortune_cookie(message);
+
+    int name = 233;
+    char named[] = "Noble Ed";
+    //printf("Size of name is: %d \n", sizeof(name));
+
+    printf("String stored at address: %p \n", named);
+}
+
+
