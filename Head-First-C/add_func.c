2f3bbb5d937c23e8465d1f8cbc206a77f64eebaf
Mokoth <onyach.okoth1@outlook.com>
2022-09-28 20:37:53 +0000

    Modification to add_func file

--- Head-First-C/add_func.c
+++ Head-First-C/add_func.c
@@ -9,5 +9,17 @@ int add_integers(int y, int x){
 int main(){
     int result = add_integers(23, 24);
     printf("Result of 23 and 24 is: %d", result);
+
+    /*
+        Getting user input and calculating the sum
+        
+        int result, val1, val2;
+        printf("Enter value 1: ");
+        scanf("%d", &val1);
+        printf("Enter value 2: ");
+        scanf("%d", &val2);
+        result = add_integers(val1, val2);
+        printf("%d", result);
+    */
     return 0;
 }
