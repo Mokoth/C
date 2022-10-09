bffffa7e10204aafe7fdf89126ecc4e28666d964
Mokoth <onyach.okoth1@outlook.com>
2022-10-09 11:02:59 +0300

    Print Output using putc()

---
+++ C Lessons/Listing 5.1/05L03.c
@@ -0,0 +1,12 @@
+/* 05LO3.c: Outputting a character with putc() */
+#include <stdio.h>
+
+main(){
+
+  int ch;
+  
+  ch = 65; /* the numeric value of A */
+  printf("The character that has numeric value of 65 is:\n");
+  putc(ch, stdout);
+  return 0;
+}
