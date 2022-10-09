5ba9be8bf9186f9d12927650edd3e30b3648cef8
Mokoth <onyach.okoth1@outlook.com>
2022-10-09 11:18:09 +0300

    Print Output using putchar()

---
+++ C Lessons/Listing 5.1/05L04.c
@@ -0,0 +1,13 @@
+/* 05L04.c: Outputting character using putchar() */
+#include <stdio.h>
+
+main(){
+
+  putchar(65);
+    putchar(10);
+      putchar(66);
+        putchar(10);
+      putchar(67);
+    putchar(10);
+  return 0;
+}
