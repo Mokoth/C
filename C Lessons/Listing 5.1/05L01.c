2fe083367758f5078e94a2a9445952b5ca0a3ce7
Mokoth <onyach.okoth1@outlook.com>
2022-10-09 09:49:06 +0300

    Deleted files

---
+++ C Lessons/Listing 5.1/05L01.c
@@ -0,0 +1,10 @@
+/* 05LO1.c: Reading input by calling getc() */
+#include <stdio.h>
+main(){
+  int ch;
+
+  printf("Please type in one character: \n");
+  ch = getc( stdin );
+  printf("The character you just entered is: %c\n", ch);
+  return 0;
+}
