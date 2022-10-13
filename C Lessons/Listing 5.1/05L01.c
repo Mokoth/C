d34a17495a775142629e30197a7be4dcc6aa3386
Mokoth <onyach.okoth1@outlook.com>
2022-10-13 22:09:23 +0300

    Update on 05LO1.c

--- C Lessons/Listing 5.1/05L01.c
+++ C Lessons/Listing 5.1/05L01.c
@@ -1,4 +1,7 @@
 /* 05LO1.c: Reading input by calling getc() */
+/* Running c file from cmd: gcc 05LO1.c, which results in an executable a.out file */
+/* Alternatively gcc 05L01.c -o hello */
+
 #include <stdio.h>
 main(){
   int ch;
